325b34b5640c81a427cc56d390e3167ca92e061a
Jan Richter <jrichter@redhat.com>
2026-02-16 14:11:03 +0100

    update e2e utils
    Signed-off-by: Jan Richter <jrichter@redhat.com>

--- workspaces/quickstart/e2e-tests/tests/specs/quick-start.spec.ts
+++ workspaces/quickstart/e2e-tests/tests/specs/quick-start.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from "rhdh-e2e-test-utils/test";
+import { test, expect } from "@red-hat-developer-hub/e2e-test-utils/test";
 
 test.describe("Test Quick Start plugin", () => {
   test.beforeAll(async ({ rhdh }) => {
