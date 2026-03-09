1702715376e44cd311af155c183cfad4a5849c3a
Sanket Saikia <sanketsaikia13@gmail.com>
2026-03-09 18:12:04 +0530

    update imports

--- workspaces/argocd/e2e-tests/eslint.config.js
+++ workspaces/argocd/e2e-tests/eslint.config.js
@@ -1,3 +1,3 @@
-import { createEslintConfig } from "rhdh-e2e-test-utils/eslint";
+import { createEslintConfig } from "@red-hat-developer-hub/e2e-test-utils/eslint";
 
 export default createEslintConfig(import.meta.dirname);
