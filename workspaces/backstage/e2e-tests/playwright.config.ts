c56a31acf34ea10953bfda5427f09cc7d98d2ff7
Patrick Knight <pknight@redhat.com>
2026-04-03 07:24:23 -0400

    chore(e2e): sonarqube and prettier fixes
    Signed-off-by: Patrick Knight <pknight@redhat.com>

--- workspaces/backstage/e2e-tests/playwright.config.ts
+++ workspaces/backstage/e2e-tests/playwright.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from '@red-hat-developer-hub/e2e-test-utils/playwright-config';
-import dotenv from 'dotenv';
+import { defineConfig } from "@red-hat-developer-hub/e2e-test-utils/playwright-config";
+import dotenv from "dotenv";
 
 dotenv.config({ path: `${import.meta.dirname}/.env` });
 
@@ -18,15 +18,15 @@ export default defineConfig({
       testMatch: /tests\/specs\/github-events-module\.spec\.ts/,
     },
     {
-      name: 'backstage-kubernetes',
+      name: "backstage-kubernetes",
       testMatch: /tests\/specs\/kubernetes-rbac\.spec\.ts/,
     },
     {
-      name: 'backstage-notifications',
+      name: "backstage-notifications",
       testMatch: /tests\/specs\/notifications\.spec\.ts/,
     },
     {
-      name: 'backstage-techdocs',
+      name: "backstage-techdocs",
       testMatch: /tests\/specs\/techdocs\.spec\.ts/,
     },
   ],
