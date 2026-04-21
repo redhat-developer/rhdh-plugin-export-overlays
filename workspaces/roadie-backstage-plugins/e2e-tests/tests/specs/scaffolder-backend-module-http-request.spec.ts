312b55e83c4201d5d39d0ee157365bdfc1f58a93
Dominik Augustín <daugusti@redhat.com>
2026-04-21 16:12:43 +0200

    test(roadie-e2e): adapt test for scaffolder-backend-module-http-request from rhdh repo
    Signed-off-by: Dominik Augustín <daugusti@redhat.com>

---
+++ workspaces/roadie-backstage-plugins/e2e-tests/tests/specs/scaffolder-backend-module-http-request.spec.ts
@@ -0,0 +1,55 @@
+import { test } from "@red-hat-developer-hub/e2e-test-utils/test";
+import { CatalogImportPage } from "@red-hat-developer-hub/e2e-test-utils/pages";
+
+// https://github.com/RoadieHQ/roadie-backstage-plugins/tree/main/plugins/scaffolder-actions/scaffolder-backend-module-http-request
+// Pre-req: Enable roadiehq-scaffolder-backend-module-http-request-dynamic plugin
+// Pre-req: Enable janus-idp-backstage-plugin-quay plugin
+test.describe("Testing scaffolder-backend-module-http-request to invoke an external request", () => {
+  test.skip(
+    () => process.env.JOB_NAME.includes("osd-gcp"),
+    "skipping due to RHDHBUGS-555 on OSD Env",
+  );
+
+  let catalogImport: CatalogImportPage;
+  const template =
+    "https://github.com/janus-qe/software-template/blob/main/test-http-request.yaml";
+
+  test.beforeAll(async ({ rhdh }) => {
+    test.info().annotations.push({
+      type: "component",
+      description: "plugins",
+    });
+
+    await rhdh.configure({ auth: "guest" });
+    await rhdh.deploy();
+  });
+
+  test.beforeEach(async ({ page, loginHelper }) => {
+    await loginHelper.loginAsGuest();
+    catalogImport = new CatalogImportPage(page);
+  });
+
+  test("Create a software template using http-request plugin", async ({
+    uiHelper,
+  }) => {
+    test.setTimeout(130000);
+    await uiHelper.clickLink({
+      ariaLabel: "Self-service",
+    });
+    await uiHelper.verifyHeading("Self-service");
+    await uiHelper.verifyHeading("Templates");
+    await uiHelper.clickButton("Import an existing Git repository");
+    await catalogImport.registerExistingComponent(template, false);
+
+    await uiHelper.openSidebar("Catalog");
+    await uiHelper.selectMuiBox("Kind", "Template");
+    await uiHelper.searchInputPlaceholder("Test HTTP Request");
+    await uiHelper.clickLink("Test HTTP Request");
+    await uiHelper.verifyHeading("Test HTTP Request");
+    await uiHelper.clickLink("Launch Template");
+    await uiHelper.verifyHeading("Self-service");
+    await uiHelper.clickButton("Create");
+    //Checking for Http Status 200
+    await uiHelper.verifyText("200", false);
+  });
+});
