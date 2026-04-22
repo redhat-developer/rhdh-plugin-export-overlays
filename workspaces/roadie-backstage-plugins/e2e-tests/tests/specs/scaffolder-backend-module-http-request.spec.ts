8e2636597ac70efb027f24837002fd4f543a393d
Dominik Augustín <daugusti@redhat.com>
2026-04-22 13:15:31 +0200

    fix(e2e): add optional chaining to JOB_NAME check in scaffolder-backend-module-http-request test
    Signed-off-by: Dominik Augustín <daugusti@redhat.com>

--- workspaces/roadie-backstage-plugins/e2e-tests/tests/specs/scaffolder-backend-module-http-request.spec.ts
+++ workspaces/roadie-backstage-plugins/e2e-tests/tests/specs/scaffolder-backend-module-http-request.spec.ts
@@ -6,7 +6,7 @@ import { CatalogImportPage } from "@red-hat-developer-hub/e2e-test-utils/pages";
 // Pre-req: Enable janus-idp-backstage-plugin-quay plugin
 test.describe("Testing scaffolder-backend-module-http-request to invoke an external request", () => {
   test.skip(
-    () => process.env.JOB_NAME.includes("osd-gcp"),
+    () => process.env?.JOB_NAME?.includes("osd-gcp") ?? false,
     "skipping due to RHDHBUGS-555 on OSD Env",
   );
 
