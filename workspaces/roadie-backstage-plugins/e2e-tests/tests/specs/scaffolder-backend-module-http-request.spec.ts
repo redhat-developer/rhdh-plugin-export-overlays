8569ca328b82aef5bdf39a6b10e53176ca8c5f4b
Dominik Augustín <daugusti@redhat.com>
2026-04-29 11:11:59 +0200

    chore(e2e): cleaned up http request config and spec
    Signed-off-by: Dominik Augustín <daugusti@redhat.com>

--- workspaces/roadie-backstage-plugins/e2e-tests/tests/specs/scaffolder-backend-module-http-request.spec.ts
+++ workspaces/roadie-backstage-plugins/e2e-tests/tests/specs/scaffolder-backend-module-http-request.spec.ts
@@ -1,19 +1,8 @@
 import { test } from "@red-hat-developer-hub/e2e-test-utils/test";
-import { CatalogImportPage } from "@red-hat-developer-hub/e2e-test-utils/pages";
 
 // https://github.com/RoadieHQ/roadie-backstage-plugins/tree/main/plugins/scaffolder-actions/scaffolder-backend-module-http-request
 // Pre-req: Enable roadiehq-scaffolder-backend-module-http-request-dynamic plugin
-// Pre-req: Enable janus-idp-backstage-plugin-quay plugin
 test.describe("Testing scaffolder-backend-module-http-request to invoke an external request", () => {
-  test.skip(
-    () => process.env?.JOB_NAME?.includes("osd-gcp") ?? false,
-    "skipping due to RHDHBUGS-555 on OSD Env",
-  );
-
-  let catalogImport: CatalogImportPage;
-  const template =
-    "https://github.com/janus-qe/software-template/blob/main/test-http-request.yaml";
-
   test.beforeAll(async ({ rhdh }) => {
     test.info().annotations.push({
       type: "component",
@@ -28,9 +17,8 @@ test.describe("Testing scaffolder-backend-module-http-request to invoke an exter
     await rhdh.deploy();
   });
 
-  test.beforeEach(async ({ page, loginHelper }) => {
+  test.beforeEach(async ({ loginHelper }) => {
     await loginHelper.loginAsGuest();
-    catalogImport = new CatalogImportPage(page);
   });
 
   test("Create a software template using http-request plugin", async ({
@@ -42,8 +30,6 @@ test.describe("Testing scaffolder-backend-module-http-request to invoke an exter
     });
     await uiHelper.verifyHeading("Self-service");
     await uiHelper.verifyHeading("Templates");
-    await uiHelper.clickButton("Import an existing Git repository");
-    await catalogImport.registerExistingComponent(template, false);
 
     await uiHelper.openSidebar("Catalog");
     await uiHelper.selectMuiBox("Kind", "Template");
