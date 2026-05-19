a1c5e030b4c4a372ee469cbe6d6a0a37881cae52
Oleksandr Andriienko <oandriie@redhat.com>
2026-05-19 05:38:14 +0300

    feat(e2e): work on e2e tests
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>

--- workspaces/bulk-import/e2e-tests/playwright.config.ts
+++ workspaces/bulk-import/e2e-tests/playwright.config.ts
@@ -7,17 +7,31 @@ import { baseConfig } from "@red-hat-developer-hub/e2e-test-utils/playwright-con
  */
 export default defineConfig({
   ...baseConfig,
+  use: {
+    ...baseConfig.use,
+    // headless: process.env.PLAYWRIGHT_HEADLESS === "0",
+    // viewport: null,
+    // launchOptions: {
+    //   args: ["--start-maximized"],
+    // },
+  },
   workers: 1,
   projects: [
     {
       name: "bulk-import",
       testMatch: "**/tests/specs/bulk-import.spec.ts",
       timeout: 30 * 60 * 1000,
+      // use: {
+      //   viewport: null,
+      // },
     },
     {
       name: "bulk-import-orchestrator",
       testMatch: "**/tests/specs/bulk-import-orchestrator.spec.ts",
       timeout: 30 * 60 * 1000,
+      // use: {
+      //   viewport: null,
+      // },
     },
   ],
 });
