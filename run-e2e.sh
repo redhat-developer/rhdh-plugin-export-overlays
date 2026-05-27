299be9f60bec7abc722a476eb109e6e3e1c156af
Jan Richter <jrichter@redhat.com>
2026-05-27 15:11:36 +0200

    debug

--- run-e2e.sh
+++ run-e2e.sh
@@ -63,7 +63,7 @@ E2E_TEST_UTILS_PATH="${E2E_TEST_UTILS_PATH:-}"
 # Pin specific e2e-test-utils version.
 E2E_TEST_UTILS_VERSION="${E2E_TEST_UTILS_VERSION:-}"
 # Git ref for e2e-test-utils: "owner/repo#branch" — clones and sets E2E_TEST_UTILS_PATH
-E2E_TEST_UTILS_GIT_REF="${E2E_TEST_UTILS_GIT_REF:-}"
+E2E_TEST_UTILS_GIT_REF="jrichter1/rhdh-e2e-test-utils#orchestrator"
 
 if [[ -n "$E2E_TEST_UTILS_GIT_REF" ]]; then
     CLONE_DIR="/tmp/rhdh-e2e-test-utils-${E2E_TEST_UTILS_GIT_REF##*#}"
