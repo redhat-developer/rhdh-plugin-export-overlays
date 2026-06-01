57b4c155a1a8b604de9aa570949bbd489fe5b586
Dominika Zemanovicova <dzemanov@redhat.com>
2026-06-01 17:45:51 +0200

    skip as serial tests
    Signed-off-by: Dominika Zemanovicova <dzemanov@redhat.com>

--- workspaces/scorecard/e2e-tests/tests/specs/scorecard.spec.ts
+++ workspaces/scorecard/e2e-tests/tests/specs/scorecard.spec.ts
@@ -108,7 +108,7 @@ test.describe.serial("Scorecard Plugin Tests", () => {
     );
   });
 
-  test("Aggregated scorecard (README file exists): drill-down and table UI", async () => {
+  test.skip("Aggregated scorecard (README file exists): drill-down and table UI", async () => {
     test.skip(
       process.env.E2E_NIGHTLY_MODE === "true",
       "fails in nightly runs https://redhat.atlassian.net/browse/RHDHBUGS-3191",
