c5b477fc2d7a3ac8e9bd8b13559bbc715c9bd656
sanketpathak <spathak@redhat.com>
2026-07-20 15:27:56 +0530

    remove the podExecTerminal.tsx

--- workspaces/tekton/plugins/tekton/overlay/src/components/PodExecTerminal/PodExecTerminal.tsx
+++
@@ -1,8 +0,0 @@
-/*
- * Stub component created as an overlay to fix CLI export error.
- * The CLI expects this component but it doesn't exist in the source.
- * This is a workaround for @red-hat-developer-hub/cli v1.10.7 bug.
- */
-export const PodExecTerminal = () => null;
-
-export default PodExecTerminal;
--- workspaces/tekton/plugins/tekton/overlay/src/components/PodExecTerminal/index.ts
+++
@@ -1,4 +0,0 @@
-/*
- * Stub module created as an overlay to fix CLI export error.
- */
-export { PodExecTerminal, PodExecTerminal as default } from './PodExecTerminal';
