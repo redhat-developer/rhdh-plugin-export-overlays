726ffc70807a52f2ec10c6d50d8a0089f8cb1f47
Sanket Saikia <sanketsaikia13@gmail.com>
2026-08-30 18:55:03 +0530

    fix

--- workspaces/scorecard/e2e-tests/tests/utils/setup.ts
+++ workspaces/scorecard/e2e-tests/tests/utils/setup.ts
@@ -56,6 +56,6 @@ export async function createScorecardContext(
   const scorecard = scorecardHelpers(page, uiHelper);
   const aggregated = aggregatedScorecardHelpers(page);
   await new LoginHelper(page).loginAsKeycloakUser();
-  await uiHelper.goToPageUrl("/", "Welcome back!");
+  await uiHelper.goToPageUrl("/catalog", "Catalog");
   return { context, page, catalog, scorecard, aggregated };
 }
